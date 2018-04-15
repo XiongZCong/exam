71874e53690229768a22d0f218f18e71e524bdfb
bug0 <634786617@qq.com>
2018-04-15 21:10:20 +0800

    util.RedisUtil3

--- src/main/java/com/xzc/mapper/UserMapper.java
+++ src/main/java/com/xzc/mapper/UserMapper.java
@@ -5,10 +5,12 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface UserMapper {
 
     @Select("select * from T_User WHERE username=#{username} and password=#{password}")
